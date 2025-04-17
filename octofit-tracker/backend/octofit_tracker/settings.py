6d2cd616973e1f60c84d1e508715312bedc57f06
sahoospecial <sahoospecial@gmail.com>
2025-04-17 09:16:10 +0000

    Update settings.py for MongoDB configuration and CORS settings

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'octofit_tracker.apps.OctofitTrackerConfig',
     'rest_framework',
+    'djongo',
     'corsheaders',
 ]
 
@@ -145,3 +146,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CORS_ALLOW_ALL_ORIGINS = True
